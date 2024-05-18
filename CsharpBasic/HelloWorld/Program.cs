f852f9bdb5549fd248cdc9ac47e9d9779dacdae1
gurumee92 <gurumee92@woowahan.com>
2024-05-18 23:21:50 +0900

    learn: CsharpBasic ch 4, 5, 6

--- CsharpBasic/HelloWorld/Program.cs
+++ CsharpBasic/HelloWorld/Program.cs
@@ -4,13 +4,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        GiftBox address = GiftBoxMaker("A 친구야 어떻게 지내고 있니", 150000);
-        Console.WriteLine($"{address.Letter} {address.Money}");
+        GiftBox box = GiftBoxMaker("A 친구야 어떻게 지내고 있니", 150000);
+        List<GiftBox> boxes = [box];
+
+        foreach (var b in boxes){
+            Console.WriteLine($"{b.Letter} {b.Money}");
+        }
+
+        boxes.Clear();
+
+        for (int i = 0; i < boxes.Count; i++) {
+            var b = boxes[i];
+            Console.WriteLine($"{b.Letter} {b.Money}");
+        }
     }
 
     static GiftBox GiftBoxMaker(string letter, int money)
     {
-        GiftBox address = new GiftBox()
+        GiftBox address = new()
         {
             Letter = letter,
             Money = money
